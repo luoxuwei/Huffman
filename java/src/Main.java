c59df0d5a598bf4240827d92153fd620c742a753
罗旭维 <xwluo@foxmail.com>
2022-04-30 04:17:02 +0800

    完成测试

--- java/src/Main.java
+++ java/src/Main.java
@@ -2,9 +2,24 @@
 // Created by 罗旭维 on 2022/4/30.
 //
 
+import java.io.IOException;
+
 public class Main {
 
     public static void main(String[] args) {
         Huffman huffman = new Huffman();
+        if (args[0].equals("e")) {
+            try {
+                huffman.encode(args[1], args[2]);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else {
+            try {
+                huffman.decode(args[1], args[2]);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
